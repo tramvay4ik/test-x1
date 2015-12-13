9a0d7590618f2a96bc068e3cd2403df95ff8546e
margo.koba <tramvay4ik14@gmail.com>
2015-12-13 17:48:21 +0200

    added new project

--- gulpfile.js
+++ gulpfile.js
@@ -1,9 +1,36 @@
 var gulp = require('gulp');
 var sass = require('gulp-sass');
+var prefixer = require('gulp-autoprefixer');
 
-gulp.task('style', function() {
-    gulp.src('./src/scss/*.scss')
-    .pipe(sass()) // добавляем префиксы - http://www.myth.io/
-    .pipe(gulp.dest('./build/css/')); // записываем css
-//    .pipe(livereload(server)); // даем команду на перезагрузку css
+var config = {
+    sassPath: './src/scss',
+    bowerDir: './bower_components'
+}
+
+gulp.task('css', function() {
+    return gulp.src(config.sassPath + '/main.scss')
+        .pipe(sass({
+            includePaths: [
+                config.bowerDir + '/bootstrap-sass/assets/stylesheets',
+                config.bowerDir + '/font-awesome/scss'
+            ],
+        })).pipe(prefixer())
+        .pipe(gulp.dest('./build/css'));
 });
+
+gulp.task('js', function() {
+    return gulp.src(config.bowerDir + '/bootstrap-sass/assets/javascripts/bootstrap.min.js')
+        .pipe(gulp.dest('./build/js'));
+});
+
+gulp.task('icons', function() {
+    return gulp.src(config.bowerDir + '/font-awesome/fonts/**.*')
+        .pipe(gulp.dest('./build/fonts'));
+});
+
+// Rerun the task when a file changes
+gulp.task('watch', function() {
+    gulp.watch(config.sassPath + '/**/*.scss', ['css']);
+});
+
+gulp.task('default', ['css', 'js', 'icons']);
